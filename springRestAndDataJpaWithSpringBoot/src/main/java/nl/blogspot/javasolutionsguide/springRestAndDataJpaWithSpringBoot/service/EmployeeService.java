3b914e43547d3e2f8a662fa7cbf42514184d3e65
MatejRitosa <ritossamatej@gmail.com>
2021-04-07 13:25:53 +0200

    Made an app, but EMPLOYEE table is not created

---
+++ springRestAndDataJpaWithSpringBoot/src/main/java/nl/blogspot/javasolutionsguide/springRestAndDataJpaWithSpringBoot/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package nl.blogspot.javasolutionsguide.springRestAndDataJpaWithSpringBoot.service;
+
+import java.util.List;
+
+import nl.blogspot.javasolutionsguide.springRestAndDataJpaWithSpringBoot.entity.Employee;
+
+
+public interface EmployeeService {
+    public List<Employee> retrieveEmployees();
+
+    public Employee getEmployee(Long employeeId);
+
+    public void saveEmployee(Employee employee);
+
+    public void deleteEmployee(Long employeeId);
+
+    public void updateEmployee(Employee employee);
+}
